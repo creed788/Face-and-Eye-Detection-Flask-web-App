f55cc385a390db3fffcfad7581197152d37dc94b
Dastgir Pathan <77855713+creed788@users.noreply.github.com>
2023-01-15 15:17:30 +0530

    Update face recognition.py

--- face recognition.py
+++ face recognition.py
@@ -7,8 +7,8 @@ def gen_frame():
   while True:
     success, frame  = camera.read()
     # body_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_upperbody.xml')
-    detector = cv2.CascadeClassifier('haarcascades\haarcascade_frontalcatface.xml')
-    # eye_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_eye_tree_eyeglasses.xml')
+    detector = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_eye_tree_eyeglasses.xml')
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector.detectMultiScale(gray, 1.1 ,3)
@@ -17,9 +17,9 @@ def gen_frame():
       cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
       roi_gray = gray[y:y+h, x:x+w]
       roi_color = frame[y:y+h, x:x+w]
-      # eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-      # for (ex, ey, ew, eh) in eyes:
-      #   cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
+      eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+      for (ex, ey, ew, eh) in eyes:
+         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
 
     ret, buffer = cv2.imencode('.jpg', frame)
     frame = buffer.tobytes()
